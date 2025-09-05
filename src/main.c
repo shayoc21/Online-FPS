d8be7cdacaa9fcabf77f54a468c6e538ceb715f3
Shay O'Connor <shaywoconnor@hotmail.com>
2025-09-05 22:58:07 +0000

    Update main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,7 @@ int main(int argc, char** argv)
 {
 	printf
 	(
-		   "\n==i hate bsp==\n"
+		   "\n=====Online-FPS==\n"
 		   "  By Shay O'Connor <github: shayoc21>\n"
 		   "=================\n\n"
 	);
